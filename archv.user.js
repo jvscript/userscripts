1a1368a314e831820746129a69d1c020fa8ac773
Victorien Plancke <fb@vito.fr.nf>
2021-06-29 09:36:09 +0200

    Add files via upload

---
+++ archv.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         archv.js
+// @version      0.1
+// @grant        none
+// @include		/^(http|https):\/\/www\.jeuxvideo\.com\/forums\/(.*)$/
+// ==/UserScript==
+
+
+let topic410 = false;
+if(document.getElementsByClassName("img-erreur")[0] && document.getElementsByClassName("img-erreur")[0].src == "https://www.jeuxvideo.com/img/erreurs/e410.png"){
+	topic410 = true;
+}
+
+if(topic410){
+    var url = document.location.href.replace("jeuxvideo.com","jvarchive.com");
+    document.location.href=url;
+}
