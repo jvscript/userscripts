b47d873e43ca2cf1fdf14aac385f48754911b85f
Victorien Plancke <fb@vito.fr.nf>
2023-03-10 21:22:29 +0100

    Update archv.user.js

--- archv.user.js
+++ archv.user.js
@@ -4,16 +4,15 @@
 // @description  Redirige les 410 vers jvarchive.com
 // @version      0.2
 // @grant        none
+// @run-at       document-body
 // @include      /^(http|https):\/\/www\.jeuxvideo\.com\/forums\/(.*)$/
 // @downloadURL  https://github.com/jvscript/userscripts/raw/main/archv.user.js
 // @updateURL    https://github.com/jvscript/userscripts/raw/main/archv.user.js
 // ==/UserScript==
 
 
-let topic410 = false;
-if(document.getElementsByClassName("img-erreur")[0] && document.getElementsByClassName("img-erreur")[0].src == "https://www.jeuxvideo.com/img/erreurs/e410.png"){
-	topic410 = true;
-}
+
+let topic410 = document.getElementsByClassName("img-erreur")[0] && document.getElementsByClassName("img-erreur")[0].src == "https://www.jeuxvideo.com/img/erreurs/e410.png";
 
 if(topic410){
     var url = document.location.href.replace("jeuxvideo.com","jvarchive.com");
