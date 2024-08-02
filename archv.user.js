d96488420aee6b2771a324d8df89a296ada3ae26
Vito <fb@vito.fr.nf>
2024-08-02 17:12:14 +0200

    Update archv.user.js

--- archv.user.js
+++ archv.user.js
@@ -3,17 +3,19 @@
 // @author       unknown
 // @description  Redirige les 410 vers jvarchive.com
 // @icon         https://jvarchive.com/favicon-32x32.png
-// @version      0.2
+// @version      0.3
 // @grant        none
 // @run-at       document-body
-// @include      /^(http|https):\/\/www\.jeuxvideo\.com\/forums\/(.*)$/
+// @include      /^(http|https):\/\/www\.jeuxvideo\.com\/(.*)$/
 // @downloadURL  https://github.com/jvscript/userscripts/raw/main/archv.user.js
 // @updateURL    https://github.com/jvscript/userscripts/raw/main/archv.user.js
 // ==/UserScript==
 
 
 
-let topic410 = document.getElementsByClassName("img-erreur")[0] && document.getElementsByClassName("img-erreur")[0].src == "https://www.jeuxvideo.com/img/erreurs/e410.png";
+let topic410 = document.getElementsByClassName("img-erreur")[0] &&
+    ( document.getElementsByClassName("img-erreur")[0].src == "https://www.jeuxvideo.com/img/erreurs/e410.png" ||
+    document.getElementsByClassName("img-erreur")[0].src == "https://www.jeuxvideo.com/img/erreurs/e404.png" );
 
 if(topic410){
     var url = document.location.href.replace("jeuxvideo.com","jvarchive.com");
