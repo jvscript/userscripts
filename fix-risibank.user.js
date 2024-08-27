b1b4291dac045fe5a8a0c45bb69c257fa041b812
Vito <fb@vito.fr.nf>
2024-08-27 09:51:09 +0200

    Update fix-risibank.user.js

--- fix-risibank.user.js
+++ fix-risibank.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name         Fix risibank
-// @namespace    http://tampermonkey.net/
+// @namespace    fix_risibank
 // @version      0.1
 // @match        https://risibank.fr/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=risibank.fr
 // @grant        GM_addStyle
+// @description  Fix risibank CSS
 // ==/UserScript==
 
-GM_addStyle ( `
+GM_addStyle (`
     .actions-right{ display: none }
     a[title="SkyCh.at"] { display: none }
-    .mt-4 {margin: 0 !important }
-` );
+    .mt-4, .mt-2 {margin: 0 !important }
+`);
