a9b9bf33dc4ec225aa3419d8c507d0860159ddde
Vito <fb@vito.fr.nf>
2025-01-09 17:23:29 +0100

    Update archv.user.js

--- archv.user.js
+++ archv.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         410 Redirect
 // @author       unknown
-// @description  Redirige les 410 vers jvarchive.com
-// @icon         https://jvarchive.com/favicon-32x32.png
-// @version      0.3
+// @description  Redirige les 410 vers jeuxvideo.to
+// @icon         https://jeuxvideo.to/favicon-32x32.png
+// @version      0.4
 // @grant        none
 // @run-at       document-body
 // @include      /^(http|https):\/\/www\.jeuxvideo\.com\/(.*)$/
@@ -18,6 +18,6 @@ let topic410 = document.getElementsByClassName("img-erreur")[0] &&
     document.getElementsByClassName("img-erreur")[0].src == "https://www.jeuxvideo.com/img/erreurs/e404.png" );
 
 if(topic410){
-    var url = document.location.href.replace("jeuxvideo.com","jvarchive.com");
+    var url = document.location.href.replace("jeuxvideo.com","jeuxvideo.to");
     document.location.href=url;
 }
